1612407d1bccd769ec7dbff7210de2bc9eb52f3d
Ketok <ketok4321@gmail.com>
2022-06-08 17:40:23 +0200

    Mossy Oak Log top texture

--- src/main/java/ketok/wilderness/data/client/WdBlockStateProvider.java
+++ src/main/java/ketok/wilderness/data/client/WdBlockStateProvider.java
@@ -3,11 +3,8 @@ package ketok.wilderness.data.client;
 import ketok.wilderness.Wilderness;
 import ketok.wilderness.registry.WdBlocks;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.client.model.generators.BlockStateProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
-
-import static ketok.wilderness.data.WdDataGenUtil.*;
 
 public class WdBlockStateProvider extends BlockStateProvider {
     public WdBlockStateProvider(DataGenerator gen, ExistingFileHelper exFileHelper) {
@@ -16,6 +13,6 @@ public class WdBlockStateProvider extends BlockStateProvider {
 
     @Override
     protected void registerStatesAndModels() {
-        axisBlock(WdBlocks.MOSSY_OAK_LOG.get(), blockTexture(WdBlocks.MOSSY_OAK_LOG.get()), suffix(blockTexture(Blocks.OAK_LOG), "_top"));
+        logBlock(WdBlocks.MOSSY_OAK_LOG.get());
     }
 }
