433aa75c6ac9e5019eb6af997a0c8d2d3c2c1af6
Ketok <ketok4321@gmail.com>
2022-07-16 12:31:52 +0200

    Fix animals walking into blackberry bushes and dying

--- src/main/java/ketok/wilderness/common/block/BlackberryBushBlock.java
+++ src/main/java/ketok/wilderness/common/block/BlackberryBushBlock.java
@@ -7,6 +7,7 @@ import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.Mob;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
@@ -15,7 +16,9 @@ import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.SweetBerryBushBlock;
 import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.pathfinder.BlockPathTypes;
 import net.minecraft.world.phys.BlockHitResult;
+import org.jetbrains.annotations.Nullable;
 
 public class BlackberryBushBlock extends SweetBerryBushBlock {
     public BlackberryBushBlock(Properties p_57249_) {
@@ -51,5 +54,11 @@ public class BlackberryBushBlock extends SweetBerryBushBlock {
     @Override
     protected boolean mayPlaceOn(BlockState state, BlockGetter level, BlockPos pos) {
         return super.mayPlaceOn(state, level, pos) || state.is(Blocks.MOSS_BLOCK);
+    }
+
+    @Nullable
+    @Override
+    public BlockPathTypes getAiPathNodeType(BlockState state, BlockGetter level, BlockPos pos, @Nullable Mob entity) {
+        return BlockPathTypes.DANGER_OTHER;
     }
 }
