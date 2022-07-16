2d15ad75fc3a1c96c2555871f80bf7cc9f7e8848
Ketok <ketok4321@gmail.com>
2022-07-16 07:36:25 +0200

    Add blackberries to balannced diet advancement

--- src/main/java/ketok/wilderness/data/server/modifiers/WdAdvancementModifiersProvider.java
+++ src/main/java/ketok/wilderness/data/server/modifiers/WdAdvancementModifiersProvider.java
@@ -3,13 +3,19 @@ package ketok.wilderness.data.server.modifiers;
 import com.teamabnormals.blueprint.common.advancement.modification.AdvancementModifierProvider;
 import com.teamabnormals.blueprint.common.advancement.modification.modifiers.CriteriaModifier;
 import ketok.wilderness.Wilderness;
+import ketok.wilderness.registry.WdItems;
 import ketok.wilderness.registry.worldgen.WdBiomes;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.RequirementsStrategy;
+import net.minecraft.advancements.critereon.ConsumeItemTrigger;
 import net.minecraft.advancements.critereon.DeserializationContext;
 import net.minecraft.advancements.critereon.LocationPredicate;
 import net.minecraft.advancements.critereon.LocationTrigger;
 import net.minecraft.data.DataGenerator;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.RegistryObject;
+
+import static ketok.wilderness.data.WdDataGenUtil.*;
 
 public class WdAdvancementModifiersProvider extends AdvancementModifierProvider {
     public WdAdvancementModifiersProvider(DataGenerator dataGenerator) {
@@ -19,6 +25,7 @@ public class WdAdvancementModifiersProvider extends AdvancementModifierProvider 
     @Override
     protected void registerEntries() {
         entrySelects("adventure/adventuring_time").addModifier(createAdventuringTime());
+        entrySelects("husbandry/balanced_diet").addModifier(createBalancedDiet());
     }
 
     private EntryBuilder<Advancement.Builder, Void, DeserializationContext> entrySelects(String name) {
@@ -28,7 +35,20 @@ public class WdAdvancementModifiersProvider extends AdvancementModifierProvider 
     private CriteriaModifier createAdventuringTime() {
         CriteriaModifier.Builder builder = CriteriaModifier.builder(this.modId);
 
-        WdBiomes.HELPER.getDeferredRegister().getEntries().forEach((biome) -> builder.addCriterion(biome.get().getRegistryName().getPath(), LocationTrigger.TriggerInstance.located(LocationPredicate.inBiome(biome.getKey()))));
+        WdBiomes.HELPER.getDeferredRegister().getEntries()
+                .forEach(biome -> builder.addCriterion(name(biome.get()), LocationTrigger.TriggerInstance.located(LocationPredicate.inBiome(biome.getKey()))));
+
+        return builder.requirements(RequirementsStrategy.AND).build();
+    }
+
+    private CriteriaModifier createBalancedDiet() {
+        CriteriaModifier.Builder builder = CriteriaModifier.builder(this.modId);
+
+        WdItems.HELPER.getDeferredRegister().getEntries().stream()
+                .map(RegistryObject::get)
+                .filter(Item::isEdible)
+                .forEach(item -> builder.addCriterion(name(item), ConsumeItemTrigger.TriggerInstance.usedItem(item)));
+
         return builder.requirements(RequirementsStrategy.AND).build();
     }
 }
